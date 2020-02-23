6d9e247452e5499c944878185560a95997b8dca7
Siim Alas <siim.alas@outlook.com>
2020-02-23 19:33:00 +0200

    Probably got claimsprincipal to populate

--- KeelepuristMain/Startup.cs
+++ KeelepuristMain/Startup.cs
@@ -12,6 +12,12 @@ using Microsoft.Extensions.Azure;
 using KeelepuristMain.Services;
 using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Security.Claims;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace KeelepuristMain
 {
@@ -30,7 +36,7 @@ namespace KeelepuristMain
             services.Configure<CookiePolicyOptions>(options =>
             {
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
@@ -77,6 +83,59 @@ namespace KeelepuristMain
             app.UseCookiePolicy();
 
             app.UseRouting();
+
+            //TODO: look if an official way to polulate ClaimsPrincipal has been released.
+            // Current code from https://stackoverflow.com/questions/42260708/azure-apps-easyauth-claims-with-net-core/42270669#42270669
+            app.Use(async (context, next) =>
+            {
+                // Create a user on current thread from provided header
+                if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+                {
+                    // Read headers from Azure
+                    var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"][0];
+                    var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"][0];
+
+                    #region extract claims via call /.auth/me
+                    //invoke /.auth/me
+                    var cookieContainer = new CookieContainer();
+                    HttpClientHandler handler = new HttpClientHandler()
+                    {
+                        CookieContainer = cookieContainer
+                    };
+                    string uriString = $"{context.Request.Scheme}://{context.Request.Host}";
+                    foreach (var c in context.Request.Cookies)
+                    {
+                        cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
+                    }
+                    string jsonResult = string.Empty;
+                    using (HttpClient client = new HttpClient(handler))
+                    {
+                        var res = await client.GetAsync($"{uriString}/.auth/me");
+                        jsonResult = await res.Content.ReadAsStringAsync();
+                    }
+
+                    //parse json
+                    var obj = JArray.Parse(jsonResult);
+                    string user_id = obj[0]["user_id"].Value<string>(); //user_id
+
+                    // Create claims id
+                    List<Claim> claims = new List<Claim>();
+                    foreach (var claim in obj[0]["user_claims"])
+                    {
+                        claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
+                    }
+
+                    // Set user in current context as claims principal
+                    var identity = new GenericIdentity(azureAppServicePrincipalIdHeader);
+                    identity.AddClaims(claims);
+                    #endregion
+
+                    // Set current thread user to identity
+                    context.User = new GenericPrincipal(identity, null);
+                };
+
+                await next.Invoke();
+            });
 
             app.UseAuthentication();
             app.UseAuthorization();
