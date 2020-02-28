50265eaec0a937629c089ac7069b0ff9ec11bf5d
Siim Alas <siim.alas@outlook.com>
2020-02-28 20:02:48 +0200

    Removed autocorrect from exercises and improved js myvalidate

--- KeelepuristMain/wwwroot/js/myValidate.js
+++ KeelepuristMain/wwwroot/js/myValidate.js
@@ -12,3 +12,15 @@ function resetBlankSpace(id) {
     let blankSpace = document.getElementById(id);
     blankSpace.classList.remove("answer-correct", "answer-wrong");
 }
+
+function submitDictation() {
+    let inputTag = document.getElementById("0");
+    let correctAnswers = inputTag.name.split("|");
+
+    if (correctAnswers.includes(inputTag.value)) {
+        inputTag.classList.add("answer-correct");
+        document.getElementById("newWordLink").classList.replace("btn-light", "btn-primary");
+    } else {
+        inputTag.classList.add("answer-wrong");
+    }
+}
